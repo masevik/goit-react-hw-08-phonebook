86a0ecf7eff5a53e2e05e20a3d753cfd2da6cacf
Serhii Malychkovych <masevik@gmail.com>
2023-02-01 19:39:50 +0200

    add Clear button

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from 'nanoid';
 import { selectFilter } from 'redux/selectors';
 import { filterContact } from 'redux/filterSlice';
-import { Input, Label } from './Filter.styled';
+import { Input, Label, Button } from './Filter.styled';
 import { Box } from 'components/Box';
 
 export const Filter = () => {
@@ -26,13 +26,22 @@ export const Filter = () => {
       mb="20px"
     >
       <Label htmlFor={filterInputId}>Find contact by name</Label>
-      <Input
-        type="text"
-        name="filter"
-        id={filterInputId}
-        value={filter}
-        onChange={onFilterChange}
-      />
+      <Box display="flex">
+        <Input
+          type="text"
+          name="filter"
+          id={filterInputId}
+          value={filter}
+          onChange={onFilterChange}
+        />
+        <Button
+          onClick={() => {
+            dispatch(filterContact(''));
+          }}
+        >
+          Clear
+        </Button>
+      </Box>
     </Box>
   );
 };
--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -10,7 +10,30 @@ export const Label = styled.label`
 
 export const Input = styled.input`
   margin-left: 20px;
+  margin-right: 10px;
   margin-bottom: 20px;
   width: 460px;
   height: 35px;
+`;
+
+export const Button = styled.button`
+  width: 71px;
+  height: 35px;
+  margin-right: 20px;
+  padding: 5px 15px;
+  color: #efb343;
+  font-size: 14px;
+  background: transparent;
+  border: 1px solid #efb343;
+  border-radius: 5px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+
+  &:hover {
+    color: #ffffff;
+    border-color: #ffffff;
+  }
+
+  &:active {
+    box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
+  }
 `;
